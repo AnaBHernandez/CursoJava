ada6f09c6c5d297768651081e1e8bf461194d34e
[AnaBHernandez] <[anahernandezfit@gmail.com]>
2024-10-01 18:20:09 +0200

    Ejercicio1 terminado

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+public class Ejercicio1 {
+    //Declarar e inicializar variables
+    public static void main(String[] args) {
+        int edad = 47; //Declaración y asignación de la variable
+        String nombre; //Declaración de la variable  
+        nombre = "Ana Belén";
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+}
