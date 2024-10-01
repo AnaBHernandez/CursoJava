e92867f3859653102f3f7fb956cb266106d32d42
[AnaBHernandez] <[anahernandezfit@gmail.com]>
2024-10-01 18:25:53 +0200

    Ejercicio5 terminado

---
+++ Ejercicio5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio5 {
+    //Leer y mostrar entrada del usuario
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Ingrese un nombre");
+        String nombre = scanner.nextLine();        
+        System.out.println("Ingresa tu edad");
+        int edad = scanner.nextInt();
+
+        System.out.println("Nombre: "+nombre+ " "+"Edad: "+edad);
+        
+
+
+
+
+
+        
+    }
+}
